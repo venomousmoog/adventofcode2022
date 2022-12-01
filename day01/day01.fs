a343e2dcd4cb6ca5a00af7be9aef0d05fca10bf0
venomousmoog <venomousmoog@users.noreply.github.com>
2022-12-01 14:31:48 -0800

    update naming scheme and script to start out days

---
+++ day01/day01.fs
@@ -0,0 +1,18 @@
+﻿open System.IO;
+
+//let lines = File.ReadLines("test.txt")
+let lines = File.ReadLines("data.txt")
+
+let split lst = 
+    let folder (grouped, accum) element = 
+        match element with
+        | "" -> (grouped@[accum], [])
+        | x -> (grouped, accum@[x])
+    fst (Seq.fold folder ([], []) lst)
+
+split lines 
+    |> Seq.map (fun elf -> Seq.sumBy int elf)
+    |> Seq.sortDescending
+    |> Seq.take 3
+    |> Seq.sum
+    |> printfn "%A"
---
+++ mkday.ps1
@@ -0,0 +1,14 @@
+param ([int] $nday)
+
+$day = '{0:d2}' -f $nday
+$root = $PSScriptRoot
+$day1 = "$root\day01"
+
+
+New-Item "$root\day$day" -ItemType Directory
+
+@("day01.fsproj", "day01.fs","data.txt","test.txt") | Foreach-Object {
+    (Get-Content "$day1\$_").Replace("day01", "day$day") 
+        | Set-Content "$root\day$day\$_".Replace("day01", "day$day")
+}
+dotnet sln "$root\adventofcode2022.sln" add "$root\day$day\day$day.fsproj"
